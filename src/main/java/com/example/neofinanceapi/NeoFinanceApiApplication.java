8ef9568f120940867c82379048612566f9c21541
Aucante <sebastien.aucante@gmail.com>
2024-04-07 19:31:27 +0200

    Init API project

---
+++ src/main/java/com/example/neofinanceapi/NeoFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.neofinanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoFinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neofinanceapi/NeoFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neofinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
